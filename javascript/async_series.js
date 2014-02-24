b2bf2763fdf8166685e3b9a757f1591102189cfc
Vitor Britto <vitorbritto@users.noreply.github.com>
2014-02-24 20:45:48 -0200

    Create async_series.js

---
+++ javascript/async_series.js
@@ -0,0 +1,37 @@
+function someFunc(foo, bar, baz, callback) {
+
+    async.series([
+
+        function (step) {
+
+            step();
+        },
+
+        function (step) {
+
+            step();
+        },
+
+        function (step) {
+
+            step();
+        },
+
+        function (step) {
+
+            step();
+        },
+
+
+        function (step) {
+
+            step();
+        }
+
+        ], 
+        function () {
+            callback();
+        }
+    );
+
+}
