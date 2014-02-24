5575ee6d1adf045e30e1d4c6b3be96ef22b58dd7
Vitor Britto <vitorbritto@users.noreply.github.com>
2014-02-24 20:37:51 -0200

    Create exemplo.sh

---
+++ shell/exemplo.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# --------------------------------------------------------
+#
+# DADOS DO PROGRAMA
+#
+# meuscript.sh - O que este script faz
+#
+# Autor: Vitor Britto <email@domain.com>
+# Admin: Vitor Britto <email@domain.com>
+#
+#
+# --------------------------------------------------------
+# 
+# DESCRIÇÃO DO PROGRAMA
+#
+# Aqui vai uma descrição mais detalhada sobre este programa,
+# quais as suas funcionalidades e o resultado esperado.
+#
+# Exemplos:
+#   $ ./meuscript.sh [argumento1, ...]
+# 
+#
+# --------------------------------------------------------
+#
+# CHANGELOG 
+#
+# v1.3 - 16/02/2014, Por: Vitor Britto
+#   - Atualização da seção X 
+#   - Correção de Y bugs
+#
+# v1.2 - 08/02/2014, Por: Chucky
+#   - Removida feature W
+#   - Alteração na seção Y
+#
+# v1.1 - 01/02/2014, Por: Costinha
+#   - Atualização da seção X 
+#   - Correção de Y bugs
+#
+# v1.0 - 01/02/2014, Por: Chuck Norris
+#   - Atualização da seção X 
+#   - Correção de Y bugs
+#
+#
+# LICENÇA: MIT/GPL/BSD 
+#
