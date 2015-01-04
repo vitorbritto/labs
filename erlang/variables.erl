21bc4c0280c4e6896decf6b0d0afda35befe5aa7
Vitor Britto <code@vitorbritto.com.br>
2015-01-04 20:01:47 -0300

    Add Erlang - Variables

---
+++ erlang/variables.erl
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% VARIABLES                    %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Num = 42.  % All variable names must start with an uppercase letter.
+
+% Erlang has single assignment variables, if you try to assign a different value
+% to the variable `Num`, you’ll get an error.
+Num = 43. % ** exception error: no match of right hand side value 43
