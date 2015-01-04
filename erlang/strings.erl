da2a783192f06974684503bcca14e4b63a779c4d
Vitor Britto <code@vitorbritto.com.br>
2015-01-04 20:01:11 -0300

    Add Erlang - Strings

---
+++ erlang/strings.erl
@@ -0,0 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% STRINGS                      %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% There are no strings in Erlang. Strings are really just lists of integers.
+% Strings are enclosed in double quotation marks (`"`).
+Name = "Hello".
+[72, 101, 108, 108, 111] = "Hello".
