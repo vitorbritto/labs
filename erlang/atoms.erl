1dc8e85c5206726ecf80153d30ba1c1d50110903
Vitor Britto <code@vitorbritto.com.br>
2015-01-04 20:01:19 -0300

    Add Erlang - Atoms

---
+++ erlang/atoms.erl
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% ATOMS                        %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Atoms, are used to represent different non-numerical constant values. Atoms
+% start with lowercase letters, followed by a sequence of alphanumeric
+% characters or the underscore (`_`) or at (`@`) sign.
+Hello = hello.
+OtherNode = example@node.
+
+% Atoms with non alphanumeric values can be written by enclosing the atoms
+% with apostrophes.
+AtomWithSpace = 'some atom with space'.
