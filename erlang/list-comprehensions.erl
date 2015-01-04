049167d38d0b1dd484f43a7484ea9f0882c730ac
Vitor Britto <code@vitorbritto.com.br>
2015-01-04 20:14:14 -0300

    Add Erlang - List Comprenhensions

---
+++ erlang/list-comprehensions.erl
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% LISTS COMPREHENSIONS       %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% List comprehensions are expressions that create lists without having to use
+% funs, maps, or filters.
+% The notation `[F(X) || X <- L]` means "the list of `F(X)` where `X` is taken
+% from the list `L`."
+L = [1,2,3,4,5].
+[2*X || X <- L].  % [2,4,6,8,10]
+% A list comprehension can have generators and filters which select subset of the generated values.
+EvenNumbers = [N || N <- [1, 2, 3, 4], N rem 2 == 0]. % [2, 4]
