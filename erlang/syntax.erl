32c65f8793b7e1f2d69d5f286452547a59232c0c
Vitor Britto <code@vitorbritto.com.br>
2015-01-04 20:01:36 -0300

    Add Erlang - Syntax

---
+++ erlang/syntax.erl
@@ -0,0 +1,27 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% COMMENTS                     %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% ONE LINE comments
+
+%% FUNCTIONS comments
+
+%%% MODULE comments
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% PUNCTUATION                  %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% There 3 types of punctuation in Erlang:
+%
+% - Commas (`,`) separate arguments in function calls, data constructors, and
+% patterns.
+%
+% - Periods (`.`) (followed by whitespace) separate entire functions and
+% expressions in the shell.
+%
+% - Semicolons (`;`) separate clauses. We find clauses in several contexts:
+% function definitions and in `case`, `if`, `try..catch` and `receive`
+% expressions.
