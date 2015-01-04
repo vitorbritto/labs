d498859e62454bc8379bd51fee129089269617a3
Vitor Britto <code@vitorbritto.com.br>
2015-01-04 20:04:40 -0300

    Add Erlang - Funs (Anonymous Functions)

---
+++ erlang/funs.erl
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% FUNS                       %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Funs are "anonymous" functions. They are called this way because they have no
+% name. However they can be assigned to variables.
+Double = fun(X) -> 2*X end. % `Double` points to an anonymous function with handle: #Fun<erl_eval.6.17052888>
+Double(2).  % 4
+
+% Functions accept funs as their arguments and can return funs.
+Mult = fun(Times) -> ( fun(X) -> X * Times end ) end.
+Triple = Mult(3).
+Triple(5).  % 15
