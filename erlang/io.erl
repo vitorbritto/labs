55ad53a4982683f107e866108f02329f0808376a
Vitor Britto <code@vitorbritto.com.br>
2015-01-09 14:01:13 -0300

    Add Erlang - I/O

---
+++ erlang/io.erl
@@ -0,0 +1,67 @@
+%% ====================================================================
+%% I/O
+%% ====================================================================
+
+% get_line Function
+io:get_line("prompt: ").
+
+% format Function
+io:format("Hello there! I\'m ~w ~w. ~n", [vitor,britto]).
+
+% columns
+io:columns().
+
+% parse_erl_form
+io:parse_erl_form().
+
+% printable_range
+io:printable_range().
+
+% put_chars
+io:put_chars().
+
+% fread
+io:fread().
+
+% read
+io:read().
+
+% fwrite
+io:fwrite().
+
+% get_chars
+io:get_chars().
+
+% request
+io:request().
+
+% get_password
+io:get_password().
+
+% rows
+io:rows().
+
+% getopts
+io:getopts().
+
+% scan_erl_exprs
+io:scan_erl_exprs().
+
+% module_info
+io:module_info().
+
+% nl
+io:nl().
+
+% scan_erl_form
+io:scan_erl_form().
+
+% parse_erl_exprs
+io:parse_erl_exprs().
+
+% setopts
+io:setopts().
+
+% write
+io:write().
+
