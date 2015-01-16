fd633b13eeafd4ebf4a6e28f743ab5218626a154
Vitor Britto <code@vitorbritto.com.br>
2015-01-16 00:18:01 -0300

    Add C - Const

---
+++ c/const.c
@@ -0,0 +1,12 @@
+///////////////////////////////////////
+// Constants
+///////////////////////////////////////
+
+
+// Constants: #define <keyword>
+#define DAYS_IN_YEAR 365
+
+// Enumeration constants are also ways to declare constants.
+// All statements must end with a semicolon
+enum days {SUN = 1, MON, TUE, WED, THU, FRI, SAT};
+// MON gets 2 automatically, TUE gets 3, etc.
