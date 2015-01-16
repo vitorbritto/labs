246ce3fad13e5408cc401d8ab58815ee4d97814e
Vitor Britto <code@vitorbritto.com.br>
2015-01-16 00:19:09 -0300

    Add C - Syntax

---
+++ c/syntax.c
@@ -0,0 +1,72 @@
+///////////////////////////////////////
+// Comments
+///////////////////////////////////////
+
+// Single-line comments start with
+// - only available in C99 and later.
+
+/*
+ * Multi-line comments look like this. They work in C89 as well.
+ */
+
+
+///////////////////////////////////////
+// Special characters
+///////////////////////////////////////
+
+/*
+'\a'; // alert (bell) character
+'\n'; // newline character
+'\t'; // tab character (left justifies text)
+'\v'; // vertical tab
+'\f'; // new page (form feed)
+'\r'; // carriage return
+'\b'; // backspace character
+'\0'; // NULL character. Usually put at end of strings in C.
+//   hello\n\0. \0 used by convention to mark end of string.
+'\\'; // backslash
+'\?'; // question mark
+'\''; // single quote
+'\"'; // double quote
+'\xhh'; // hexadecimal number. Example: '\xb' = vertical tab character
+'\0oo'; // octal number. Example: '\013' = vertical tab character
+
+//print formatting:
+"%d";    // integer
+"%3d";   // integer with minimum of length 3 digits (right justifies text)
+"%s";    // string
+"%f";    // float
+"%ld";   // long
+"%3.2f"; // minimum 3 digits left and 2 digits right decimal float
+"%7.4s"; // (can do with strings too)
+"%c";    // char
+"%p";    // pointer
+"%x";    // hexadecimal
+"%o";    // octal
+"%%";    // prints %
+*/
+
+
+///////////////////////////////////////
+// Order of Evaluation
+///////////////////////////////////////
+
+//---------------------------------------------------//
+//        Operators                  | Associativity //
+//---------------------------------------------------//
+// () [] -> .                        | left to right //
+// ! ~ ++ -- + = *(type)sizeof       | right to left //
+// * / %                             | left to right //
+// + -                               | left to right //
+// << >>                             | left to right //
+// < <= > >=                         | left to right //
+// == !=                             | left to right //
+// &                                 | left to right //
+// ^                                 | left to right //
+// |                                 | left to right //
+// &&                                | left to right //
+// ||                                | left to right //
+// ?:                                | right to left //
+// = += -= *= /= %= &= ^= |= <<= >>= | right to left //
+// ,                                 | left to right //
+//---------------------------------------------------//
