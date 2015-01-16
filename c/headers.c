e85aa87bfc988842aff30a2f0e773c3699f047af
Vitor Britto <code@vitorbritto.com.br>
2015-01-16 00:18:35 -0300

    Add C - Headers

---
+++ c/headers.c
@@ -0,0 +1,13 @@
+///////////////////////////////////////
+// Headers
+///////////////////////////////////////
+
+
+// Import headers with #include
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// (File names between <angle brackets> are headers from the C standard library.)
+// For your own headers, use double quotes instead of angle brackets:
+//#include "my_header.h"
