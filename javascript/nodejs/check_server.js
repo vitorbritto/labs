cdc8766aaae88a431474f3f5915f446bc8cde67a
Vitor Britto <code@vitorbritto.com.br>
2015-06-25 10:06:58 -0300

    Update NodeJS Structure

--- javascript/nodejs/check_server.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-
-http.get({host: "vitorbritto.com.br"}, function(res) {
-    if (res.statusCode === 200) {
-        console.log("This site is up and runnning!");
-    } else {
-        console.log("This site might be down!");
-    }
-});
